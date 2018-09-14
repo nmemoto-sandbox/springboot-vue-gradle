d8923293cbf9a2f330cb864f93bc074e84143176
Takafumi Umemoto <umecch@gmail.com>
2018-09-14 23:40:23 +0900

    初期コミット

---
+++ src/main/java/jp/nmemoto/springbootvue/SpringbootVueApplication.java
@@ -0,0 +1,12 @@
+package jp.nmemoto.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueApplication.class, args);
+    }
+}
